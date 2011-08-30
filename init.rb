d5396e5c4a722184f09c9c760ba4064defc2d0b6
Txinto Vaz <rabioso@gatatac.org>
2011-08-30 19:41:31 +0100

    Changed gatatac.net:3001 to gatatac.net

--- init.rb
+++ init.rb
@@ -5,8 +5,8 @@ Redmine::Plugin.register :redmine_wiki_books do
   author 'Txinto Vaz'
   description 'This is a plugin for Redmine that allows the user to read books written in wiki pages'
   version '0.0.1'
-  url 'http://gatatac.net:3001/projects/redminewikibooks'
-  author_url 'http://gatatac.net:3001/users/3'
+  url 'http://gatatac.net/projects/redminewikibooks'
+  author_url 'http://gatatac.net/users/3'
 
    permission :view_books, :books => [:index,:show]
    permission :manage_books, {:books => [:new,:create,:add_book_chapter,:edit,:destroy],:book_chapters => [:new,:create,:add_book_chapter,:edit,:destroy]}
