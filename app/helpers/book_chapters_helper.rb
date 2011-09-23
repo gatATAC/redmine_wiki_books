d380388bcd40ff0fb50243c78282195381583081
Txinto Vaz <txinto@gmail.com>
2011-09-23 18:12:07 +0100

    We solve #2 #6

--- app/helpers/book_chapters_helper.rb
+++ app/helpers/book_chapters_helper.rb
@@ -8,5 +8,21 @@ module BookChaptersHelper
       render :partial => 'book_chapters/links', :locals => {:book_chapters => book.book_chapters, :options => options}
     end
   end
+
+  # Display a link if user is authorized
+  #
+  # @param [String] name Anchor text (passed to link_to)
+  # @param [Hash] options Hash params. This will checked by authorize_for to see if the user is authorized
+  # @param [optional, Hash] html_options Options passed to link_to
+  # @param [optional, Hash] parameters_for_method_reference Extra parameters for link_to
+  def link_label_to_if_authorized(label, name, rejection_text, options = {}, html_options = nil, *parameters_for_method_reference)
+      ret=link_to_if_authorized(name, options, html_options, *parameters_for_method_reference) 
+      if (!ret.nil?) then
+        ret=label+" "+ret
+      else
+        ret=rejection_text
+      end
+      ret
+  end
   
 end
